05e4983e24ce887881790b2fa40c41417a5242ce
Beibei Wang <35056382+wbb121@users.noreply.github.com>
2023-09-28 20:50:58 +0800

    Update figures.R

--- figures.R
+++ figures.R
@@ -1,17 +1,25 @@
 ######################################################################################################################
-### figures
-### 2023/06/05
+### figures for normalization methods comparison in binary phenotype prediction 
+### 2023/09/20
 ######################################################################################################################
 
 setwd("/home/wangbb/normalization_comparison")
 
 ### packages
 all(sapply(c("vegan","ape","reshape2","ggplot2","ggtree","viridis","RColorBrewer","ggsci","aplot","ggpubr",
-             "grid","ggplotify","patchwork"),
-           require, character.only=TRUE))
+             "grid","ggplotify","patchwork"),require, character.only=TRUE))
+#load("new_figures.RData")
 
-### load the helper function
-source("helper.R")
+#======================================================================================================================#
+### function for merging two count table
+merge.func <- function(table1,table2){
+  table <- merge(table1,table2,by="row.names",all=T)
+  rownames(table) <- table$Row.names
+  table <- table[,-grep("Row.names",colnames(table))]
+  table[is.na(table)] <- 0
+  return(table)
+}
+
 
 #=====================================================================================================================#
 ### basic statistical analysis for CRC data ###
@@ -42,6 +50,13 @@ crc_pcoa_scatter <- ggscatter(crc_pcoa_df,x="Axis.1",y="Axis.2",color="study_nam
 crc_pcoa_scatter
 
 # permanova analysis
+permanova.func <- function(distance,meta_df,variable){
+  meta_df <- meta_df[!is.na(meta_df[,variable]),]
+  distance <- as.dist(as.matrix(distance)[meta_df$sample_id,meta_df$sample_id])
+  formulas <- as.formula(paste0("distance~",variable))
+  permanova <- adonis2(formulas,data=meta_df,permutations=1000,parallel=1)
+  c(variable=variable,r2=round(permanova[variable,"R2"],3),pvalue=round(permanova[variable,"Pr(>F)"],3))
+}
 crc_study_permanova <- permanova.func(distance=crc_distance,meta_df=crc_meta,variable="study_name")
 crc_study_r2 <- sprintf("italic(R^2) == %.3f",as.numeric(crc_study_permanova["r2"]))
 crc_study_pval <- sprintf("italic(p) == %.3f",as.numeric(crc_study_permanova["pvalue"]))
@@ -78,7 +93,7 @@ crc_distance_heatmap <- ggplot(crc_distance_table_long, aes(x=study2, y=study1, 
 crc_distance_heatmap
 
 ### arrange the plots
-ggarrange(crc_pcoa_scatter,crc_distance_heatmap,nrow=1,ncol=2,labels=LETTERS[1:2],widths=c(1,1.2))
+ggarrange(crc_pcoa_scatter,crc_distance_heatmap,nrow=1,ncol=2,labels=letters[1:2],widths=c(1,1.2))
 ggsave("crc_analysis.jpg",width=12,height=5,dpi=300)
 
 
@@ -101,6 +116,7 @@ ibd_pcoa_df <- merge(ibd_pcoa_df,ibd_meta,by="sample_id")
 ibd_pro1 <- as.numeric(sprintf("%.3f",ibd_pcoa$values[,"Relative_eig"][1]))*100  
 ibd_pro2 <- as.numeric(sprintf("%.3f",ibd_pcoa$values[,"Relative_eig"][2]))*100
 # plot
+ibd_pcoa_df$Axis.2 <- -ibd_pcoa_df$Axis.2
 ibd_pcoa_scatter <- ggscatter(ibd_pcoa_df,x="Axis.1",y="Axis.2",color="study_name_simplified",fill="study_name_simplified",
                               ellipse=TRUE,mean.point=FALSE,star.plot=TRUE)+
   labs(x=paste0("PCoA1(",ibd_pro1,"%)"), y=paste0("PCoA2(",ibd_pro2,"%)"),title="PCoA")+
@@ -116,12 +132,12 @@ ibd_study_r2 <- sprintf("italic(R^2) == %.3f",as.numeric(ibd_study_permanova["r2
 ibd_study_pval <- sprintf("italic(p) == %.3f",as.numeric(ibd_study_permanova["pvalue"]))
 ibd_study_labels <- data.frame(r2=ibd_study_r2,pvalue=ibd_study_pval,stringsAsFactors = FALSE)
 ibd_pcoa_scatter <- ibd_pcoa_scatter+
-  geom_text(data=ibd_study_labels,mapping=aes(x=0.35,y=-0.4,label=r2),parse=TRUE,inherit.aes=FALSE,hjust=0,size=4)+
-  geom_text(data=ibd_study_labels,mapping=aes(x=0.35,y=-0.45,label=pvalue),parse=TRUE,inherit.aes=FALSE,hjust=0,size=4)
+  geom_text(data=ibd_study_labels,mapping=aes(x=0.35,y=-0.5,label=r2),parse=TRUE,inherit.aes=FALSE,hjust=0,size=4)+
+  geom_text(data=ibd_study_labels,mapping=aes(x=0.35,y=-0.55,label=pvalue),parse=TRUE,inherit.aes=FALSE,hjust=0,size=4)
 ibd_pcoa_scatter
 
 ### heatmaps of bray curtis distance between different studies
-ibd_studies <- unique(ibd_meta$study_name_simplified)   # ibd studies
+ibd_studies <- levels(ibd_meta$study_name_simplified)   # ibd studies
 ibd_distance <- as.matrix(ibd_distance)
 ibd_distance_table <- matrix(NA,nrow=length(ibd_studies),ncol=length(ibd_studies),dimnames=list(ibd_studies,ibd_studies))
 for(i in 1:length(ibd_studies)){
@@ -147,16 +163,18 @@ ibd_distance_heatmap <- ggplot(ibd_distance_table_long, aes(x=study2, y=study1, 
 ibd_distance_heatmap
 
 ### arrange the plots
-ggarrange(ibd_pcoa_scatter,ibd_distance_heatmap,nrow=1,ncol=2,labels=LETTERS[1:2],widths=c(1,1.2))
+ggarrange(ibd_pcoa_scatter,ibd_distance_heatmap,nrow=1,ncol=2,labels=letters[1:2],widths=c(1,1.2))
 ggsave("ibd_analysis.jpg",width=12,height=5,dpi=300)
 
 
 #=====================================================================================================================#
-### heatmap for simulation results ###
+### simulation scenario1 ###
 #=====================================================================================================================#
 ### data 
 norm_methods <- readRDS("data/norm_methods.rds")
-sim_pred_summ <- readRDS("data/sim_pred_summ.rds")
+scenario1_summ <- readRDS("data/scenario1_res_summ.rds")
+scenario1_summ <- merge(scenario1_summ,norm_methods,by="norm_method")
+scenario1_summ$ep <- factor(scenario1_summ$ep,levels=c(0,0.05,0.1,0.15,0.2,0.25,0.4,0.6,0.8,1))
 
 ### margins of normalization methods
 margins <- ggplot(norm_methods,aes("",annotated_method,fill=class))+
@@ -168,33 +186,116 @@ margins <- ggplot(norm_methods,aes("",annotated_method,fill=class))+
 margins
 
 ### heatmap
-heatmap.func <- function(df_data,ed,title){
+heatmap.scenario1.func <- function(df_data,ed,title){
   # heatmaps
   df_data <- df_data[df_data$ed==ed,]
   df_data <- df_data[df_data$norm_method%in%norm_methods$norm_method,]
-  df_data$alpha <- factor(paste0("ep=",df_data$alpha))
   df_data$average_auc <- round(df_data$average_auc,3)
-  heatmaps <- ggplot(df_data, aes(x=alpha, y=annotated_method, fill=average_auc))+ 
+  heatmaps <- ggplot(df_data, aes(x=ep, y=annotated_method, fill=average_auc))+ 
     geom_tile() +
-    geom_text(aes(x=alpha, y=annotated_method, label=average_auc), color="black", size=4)+
-    labs(x="",y="",fill="Average\nAUC",title=title)+
-    scale_fill_distiller(palette="Reds",limits=c(0.4,1),direction=-1)+
-    scale_x_discrete(position="top")+
+    geom_text(aes(x=ep, y=annotated_method, label=average_auc), color="black", size=4)+
+    labs(x="population effect",y="",fill="Average\nAUC",title=title)+
+    scale_fill_distiller(palette="Reds",limits=c(0.4,1),direction=1)+
     theme_bw()+
     theme(panel.grid.minor=element_blank(), panel.border=element_blank())
   heatmaps
 }
-p1 <- heatmap.func(df_data=sim_pred_summ,ed=1.02,title=expression(paste(bold("A")," ed = 1.02")))
-p2 <- heatmap.func(df_data=sim_pred_summ,ed=1.04,title=expression(paste(bold("B")," ed = 1.04")))+theme(axis.text.y=element_blank())
-p3 <- heatmap.func(df_data=sim_pred_summ,ed=1.06,title=expression(paste(bold("C")," ed = 1.06")))+theme(axis.text.y=element_blank())
+p1 <- heatmap.scenario1.func(df_data=scenario1_summ,ed=1.02,title=expression(paste(bold("a")," ed = 1.02")))
+p2 <- heatmap.scenario1.func(df_data=scenario1_summ,ed=1.04,title=expression(paste(bold("b")," ed = 1.04")))+theme(axis.text.y=element_blank())
+p3 <- heatmap.scenario1.func(df_data=scenario1_summ,ed=1.06,title=expression(paste(bold("c")," ed = 1.06")))+theme(axis.text.y=element_blank())
 p1 %>% insert_left(margins,width=0.05) %>% 
   insert_right(p2, width=1) %>%
   insert_right(p3,width=1)
-ggsave("sim_pred_heatmap.jpg",width=18,height=6,dpi=300)
+ggsave("sim_scenario1.jpg",width=18,height=6,dpi=300)
 
 
 #=====================================================================================================================#
-### prediction results for CRC datasets ###
+### simulation scenario2 ###
+#=====================================================================================================================#
+### data 
+norm_methods <- readRDS("data/norm_methods.rds")
+scenario2_summ <- readRDS("data/scenario2_res_summ.rds")
+scenario2_summ <- merge(scenario2_summ,norm_methods,by="norm_method")
+scenario2_summ$factor <- factor(paste0("m",scenario2_summ$batch_mean,"v",scenario2_summ$batch_var),
+                                levels=c("m0v1","m0v2","m0v4","m500v1","m1000v1"))
+
+### margins of normalization methods
+margins <- ggplot(norm_methods,aes("",annotated_method,fill=class))+
+  geom_tile() + 
+  scale_fill_jco()+
+  labs(fill="Group of\nnormalization\nmethods")+
+  theme_minimal()+xlab(NULL) + ylab(NULL) +
+  theme(axis.text.y=element_blank())
+margins
+
+### heatmap
+heatmap.scenario2.func <- function(df_data,ed,title){
+  # heatmaps
+  df_data <- df_data[df_data$ed==ed,]
+  df_data <- df_data[df_data$norm_method%in%norm_methods$norm_method,]
+  df_data$average_auc <- round(df_data$average_auc,3)
+  heatmaps <- ggplot(df_data, aes(x=factor, y=annotated_method, fill=average_auc))+ 
+    geom_tile() +
+    geom_text(aes(x=factor, y=annotated_method, label=average_auc), color="black", size=4)+
+    labs(x="factor",y="",fill="Average\nAUC",title=title)+
+    scale_fill_distiller(palette="Reds",limits=c(0.4,1),direction=1)+
+    theme_bw()+
+    theme(panel.grid.minor=element_blank(), panel.border=element_blank())
+  heatmaps
+}
+p1 <- heatmap.scenario2.func(df_data=scenario2_summ,ed=1.02,title=expression(paste(bold("a")," ed = 1.02")))
+p2 <- heatmap.scenario2.func(df_data=scenario2_summ,ed=1.04,title=expression(paste(bold("b")," ed = 1.04")))+theme(axis.text.y=element_blank())
+p3 <- heatmap.scenario2.func(df_data=scenario2_summ,ed=1.06,title=expression(paste(bold("c")," ed = 1.06")))+theme(axis.text.y=element_blank())
+p1 %>% insert_left(margins,width=0.05) %>% 
+  insert_right(p2, width=1) %>%
+  insert_right(p3,width=1)
+ggsave("sim_scenario2.jpg",width=18,height=6,dpi=300)
+
+
+#=====================================================================================================================#
+### simulation scenario3 ###
+#=====================================================================================================================#
+### data 
+norm_methods <- readRDS("data/norm_methods.rds")
+scenario3_summ <- readRDS("data/scenario3_res_summ.rds")
+scenario3_summ <- merge(scenario3_summ,norm_methods,by="norm_method")
+scenario3_summ$overlap <- factor(scenario3_summ$overlap,levels=c(2,4,6,8,10))
+
+### margins of normalization methods
+margins <- ggplot(norm_methods,aes("",annotated_method,fill=class))+
+  geom_tile() + 
+  scale_fill_jco()+
+  labs(fill="Group of\nnormalization\nmethods")+
+  theme_minimal()+xlab(NULL) + ylab(NULL) +
+  theme(axis.text.y=element_blank())
+margins
+
+### heatmap
+heatmap.scenario3.func <- function(df_data,ed,title){
+  # heatmaps
+  df_data <- df_data[df_data$ed==ed,]
+  df_data <- df_data[df_data$norm_method%in%norm_methods$norm_method,]
+  df_data$average_auc <- round(df_data$average_auc,3)
+  heatmaps <- ggplot(df_data, aes(x=overlap, y=annotated_method, fill=average_auc))+ 
+    geom_tile() +
+    geom_text(aes(x=overlap, y=annotated_method, label=average_auc), color="black", size=4)+
+    labs(x="overlap",y="",fill="Average\nAUC",title=title)+
+    scale_fill_distiller(palette="Reds",limits=c(0.56,1),direction=1)+
+    theme_bw()+
+    theme(panel.grid.minor=element_blank(), panel.border=element_blank())
+  heatmaps
+}
+p1 <- heatmap.scenario3.func(df_data=scenario3_summ,ed=1.02,title=expression(paste(bold("a")," ed = 1.02")))
+p2 <- heatmap.scenario3.func(df_data=scenario3_summ,ed=1.04,title=expression(paste(bold("b")," ed = 1.04")))+theme(axis.text.y=element_blank())
+p3 <- heatmap.scenario3.func(df_data=scenario3_summ,ed=1.06,title=expression(paste(bold("c")," ed = 1.06")))+theme(axis.text.y=element_blank())
+p1 %>% insert_left(margins,width=0.05) %>% 
+  insert_right(p2, width=1) %>%
+  insert_right(p3,width=1)
+ggsave("sim_scenario3.jpg",width=18,height=6,dpi=300)
+
+
+#=====================================================================================================================#
+### CRC data ###
 #=====================================================================================================================#
 ### data 
 crc_pred <- readRDS("data/crc_pred.rds")
@@ -254,7 +355,7 @@ box_crc_rank <- ggboxplot(crc_pred_summ,x="annotated_method",y="order_average_au
 box_crc_rank
 
 # combine box_auc_template and box_crc_rank
-ggarrange(box_auc_template,box_crc_rank,ncol=1,labels=LETTERS[1:2],common.legend=T)
+ggarrange(box_auc_template,box_crc_rank,ncol=1,labels=letters[1:2],common.legend=T)
 ggsave("crc_pred_res.jpg",width=10,height=5,dpi=300)
 
 
@@ -305,5 +406,20 @@ ggboxplot(ibd_pred_summ,x="annotated_method",y="order_average_auc",color="class"
   theme(axis.text.x=element_text(vjust=0.5,hjust=1,angle=90))+
   theme(legend.position="top")
 ggsave("ibd_pred_rank.jpg",width=10,height=3,dpi=300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
