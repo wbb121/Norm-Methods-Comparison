020657b473c5cb23006c78681c6ae93525f09101
Beibei Wang <35056382+wbb121@users.noreply.github.com>
2024-03-13 11:36:50 +0800

    Update pred_res_summ.R

--- pred_res_summ.R
+++ pred_res_summ.R
@@ -1,6 +1,6 @@
 ######################################################################################################################
 ### summarize the results
-### 2023/09/28
+### 2024/03/12
 ######################################################################################################################
 
 setwd("/home/wangbb/normalization_comparison")
@@ -38,15 +38,20 @@ norm_methods <- readRDS("data/norm_methods.rds")
 pred_method <- "rfr"
 
 ### prediction results
-scenario1_df <- data.frame(ep=numeric(0),ed=numeric(0),norm_method=character(0),pred_method=character(0),auc=numeric(0))
-scenario1_summ_df <- data.frame(ep=numeric(0),ed=numeric(0),norm_method=character(0),pred_method=character(0),average_auc=numeric(0))
+scenario1_df <- data.frame(ep=numeric(0),ed=numeric(0),norm_method=character(0),pred_method=character(0),
+                           auc=numeric(0),accuracy=numeric(0),specificity=numeric(0),sensitivity=numeric(0))
+scenario1_summ_df <- data.frame(ep=numeric(0),ed=numeric(0),norm_method=character(0),pred_method=character(0),
+                                average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0))
 for(alpha in population_effects){
   for(disease_effect in disease_effects){
     for(norm_method in norm_methods$norm_method){
       #alpha=0;disease_effect=1.02;norm_method="TSS"
       df <- readRDS(paste0("scenario1/pred_results/",norm_method,"/norm_alpha",alpha,"_ed",disease_effect,"_",norm_method,"_",pred_method,".rds"))
       summ_df <- data.frame(ep=alpha,ed=disease_effect,norm_method=norm_method,pred_method=pred_method,
-                            average_auc=round(mean(as.numeric(df$auc_value)),3))
+                            average_auc=round(mean(as.numeric(df$auc_value)),3),
+                            average_accuracy=round(mean(as.numeric(df$accuracy)),3),
+                            average_specificity=round(mean(as.numeric(df$specificity)),3),
+                            average_sensitivity=round(mean(as.numeric(df$sensitivity)),3))
       scenario1_df <- rbind(scenario1_df,df)
       scenario1_summ_df <- rbind(scenario1_summ_df,summ_df)
     }
@@ -66,17 +71,21 @@ norm_methods <- readRDS("data/norm_methods.rds")
 pred_method <- "rfr"
 
 ### prediction results
-scenario2_df <- data.frame(ed=numeric(0),batch_mean=numeric(0),batch_var=numeric(0),
-                           norm_method=character(0),pred_method=character(0),auc=numeric(0))
-scenario2_summ_df <- data.frame(ed=numeric(0),batch_mean=numeric(0),batch_var=numeric(0),
-                                norm_method=character(0),pred_method=character(0),average_auc=numeric(0))
+scenario2_df <- data.frame(ed=numeric(0),batch_mean=numeric(0),batch_var=numeric(0),norm_method=character(0),pred_method=character(0),
+                           auc=numeric(0),accuracy=numeric(0),specificity=numeric(0),sensitivity=numeric(0))
+scenario2_summ_df <- data.frame(ed=numeric(0),batch_mean=numeric(0),batch_var=numeric(0),norm_method=character(0),pred_method=character(0),
+                                average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0))
 for(i in 1:length(parameters)){
   for(disease_effect in disease_effects){
     for(norm_method in norm_methods$norm_method){
       #alpha=0;disease_effect=1.02;norm_method="TSS"
       df <- readRDS(paste0("scenario2/pred_results/",norm_method,"/pred_ed",disease_effect,"_mean",parameters[[i]][1],"_var",parameters[[i]][2],"_",norm_method,"_",pred_method,".rds"))
       summ_df <- data.frame(ed=disease_effect,batch_mean=parameters[[i]][1],batch_var=parameters[[i]][2],
-                            norm_method=norm_method,pred_method=pred_method,average_auc=round(mean(as.numeric(df$auc_value)),3))
+                            norm_method=norm_method,pred_method=pred_method,
+                            average_auc=round(mean(as.numeric(df$auc_value)),3),
+                            average_accuracy=round(mean(as.numeric(df$accuracy)),3),
+                            average_specificity=round(mean(as.numeric(df$specificity)),3),
+                            average_sensitivity=round(mean(as.numeric(df$sensitivity)),3))
       scenario2_df <- rbind(scenario2_df,df)
       scenario2_summ_df <- rbind(scenario2_summ_df,summ_df)
     }
@@ -96,14 +105,19 @@ norm_methods <- readRDS("data/norm_methods.rds")
 pred_method <- "rfr"
 
 ### prediction results
-scenario3_df <- data.frame(ed=numeric(0),overlap=character(0),norm_method=character(0),pred_method=character(0),auc=numeric(0))
-scenario3_summ_df <- data.frame(ed=numeric(0),overlap=character(0),norm_method=character(0),pred_method=character(0),average_auc=numeric(0))
+scenario3_df <- data.frame(ed=numeric(0),overlap=character(0),norm_method=character(0),pred_method=character(0),
+                           auc=numeric(0),accuracy=numeric(0),specificity=numeric(0),sensitivity=numeric(0))
+scenario3_summ_df <- data.frame(ed=numeric(0),overlap=character(0),norm_method=character(0),pred_method=character(0),
+                                average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0))
 for(overlap in overlaps){
   for(disease_effect in disease_effects){
     for(norm_method in norm_methods$norm_method){
       df <- readRDS(paste0("scenario3/pred_results/",norm_method,"/pred_ed",disease_effect,"_overlap",overlap,"_",norm_method,"_",pred_method,".rds"))
       summ_df <- data.frame(ed=disease_effect,overlap=overlap,norm_method=norm_method,pred_method=pred_method,
-                            average_auc=round(mean(as.numeric(df$auc_value)),3))
+                            average_auc=round(mean(as.numeric(df$auc_value)),3),
+                            average_accuracy=round(mean(as.numeric(df$accuracy)),3),
+                            average_specificity=round(mean(as.numeric(df$specificity)),3),
+                            average_sensitivity=round(mean(as.numeric(df$sensitivity)),3))
       scenario3_df <- rbind(scenario3_df,df)
       scenario3_summ_df <- rbind(scenario3_summ_df,summ_df)
     }
@@ -121,53 +135,58 @@ crc_meta <- readRDS("data/crc_meta.rds")
 crc_studies <- unique(crc_meta$study_name)
 norm_methods <- readRDS("data/norm_methods.rds")
 
-### results of cross-study prediction using ranger
-crc_pred <- data.frame(study1=NA,study2=NA,norm_method=NA,pred_method=NA,auc_values=NA)
-crc_pred <- crc_pred[-1,]
-for(i in norm_methods$norm_method){
-  for(j in crc_studies){
-    for(k in crc_studies){
-      if(j!=k){
-        pred_res_file <- paste0("./real_data_pred/",pred_method,"_",i,"_trn_",j,"_tst_",k,".rds")
-        pred_res <- readRDS(pred_res_file)
-        crc_pred <- rbind(crc_pred,pred_res)
+### prediction results of cross-study prediction using ranger
+crc_df <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                     auc=numeric(0),accuracy=numeric(0),specificity=numeric(0),sensitivity=numeric(0))
+crc_summ_df <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                          average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0))
+for(study1 in crc_studies){
+  for(study2 in crc_studies){
+    for(norm_method in norm_methods$norm_method){
+      if(study1 != study2){
+        # study1="FengQ_2015";study2="GuptaA_2019";norm_method="TSS";pred_method="ranger"
+		df <- readRDS(paste0("./real_data_pred/",pred_method,"_",norm_method,"_trn_",study1,"_tst_",study2,".rds"))
+        df$accuracy <- 1-df$misclassification_rate
+        summ_df <- data.frame(study1=study1,study2=study2,norm_method=norm_method,pred_method=pred_method,
+                              average_auc=round(mean(as.numeric(df$auc_value)),3),
+                              average_accuracy=round(mean(as.numeric(df$accuracy)),3),
+                              average_specificity=round(mean(as.numeric(df$specificity)),3),
+                              average_sensitivity=round(mean(as.numeric(df$sensitivity)),3))
+        crc_df <- rbind(crc_df,df)
+        crc_summ_df <- rbind(crc_summ_df,summ_df)
       }
     }
   }
 }
-# save the results
-saveRDS(crc_pred,"data/crc_pred.rds")
+saveRDS(crc_df,"data/crc_pred.rds")
+saveRDS(crc_summ_df,"data/crc_pred_summ.rds")
 
-### compute the mean auc value for different combinations
-crc_pred_summ <- data.frame(study1=NA,study2=NA,pred_method=NA,norm_method=NA,average_auc=NA)
-crc_pred_summ <- crc_pred_summ[-1,]
-for(i in unique(crc_pred$study1)){
-  for(j in unique(crc_pred$study2)){
-    for(k in unique(crc_pred$norm_method)){
-      data <- crc_pred[crc_pred$study1==i & crc_pred$study2==j & crc_pred$norm_method==k, ]
-      df <- data.frame(study1=i,study2=j,pred_method="crc",norm_method=k,
-                       average_auc=round(mean(as.numeric(data$auc_values)),5))
-      crc_pred_summ <- rbind(crc_pred_summ,df)
+### add the ranks according to the average auc, accuracy, specificity, sensitivity
+crc_summ_final <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                             average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0),
+                             order_auc=numeric(0),order_accuracy=numeric(0),order_specificity=numeric(0),order_sensitivity=numeric(0))
+for(study1 in crc_studies){
+  for(study2 in crc_studies){
+    if(study1 != study2){
+      df <- crc_summ_df[crc_summ_df$study1==study1 & crc_summ_df$study2==study2,]
+      # orders according to the descending order of average auc
+      df <- df[order(df$average_auc,decreasing=TRUE),]
+      df$order_auc <- 1:nrow(df)
+      # orders according to the descending order of average accuracy
+      df <- df[order(df$average_accuracy,decreasing=TRUE),]
+      df$order_accuracy <- 1:nrow(df)
+      # orders according to the descending order of average specificity
+      df <- df[order(df$average_specificity,decreasing=TRUE),]
+      df$order_specificity <- 1:nrow(df)
+      # orders according to the descending order of average sensitivity
+      df <- df[order(df$average_sensitivity,decreasing=TRUE),]
+      df$order_sensitivity <- 1:nrow(df)
+      # combine the results
+      crc_summ_final <- rbind(crc_summ_final,df)
     }
   }
 }
-
-### add the orders of auc_values
-final_data <- data.frame(study1=NA,study2=NA,pred_method=NA,norm_method=NA,average_auc=NA,order_average_auc=NA)
-final_data <- final_data[-1,]
-for(i in unique(crc_pred_summ$study1)){
-  for(j in unique(crc_pred_summ$study2)){
-    # i="FengQ_2015";j="ZellerG_2014"
-    data <- crc_pred_summ[crc_pred_summ$study1==i & crc_pred_summ$study2==j,]
-    # orders for average auc
-    data <- data[order(data$average_auc,decreasing=T),]
-    data$order_average_auc <- 1:nrow(data)
-    # final data
-    final_data <- rbind(final_data,data)
-  }
-}
-# save the results
-saveRDS(final_data,"data/crc_pred_summ.rds")
+saveRDS(crc_summ_final,"data/crc_pred_summ.rds")
 
 
 #=====================================================================================================================#
@@ -179,52 +198,57 @@ ibd_studies <- unique(ibd_meta$study_name)
 norm_methods <- readRDS("data/norm_methods.rds")
 
 ### results of cross-study prediction using ranger
-ibd_pred <- data.frame(study1=NA,study2=NA,norm_method=NA,pred_method=NA,auc_values=NA)
-ibd_pred <- ibd_pred[-1,]
-for(i in norm_methods$norm_method){
-  for(j in ibd_studies){
-    for(k in ibd_studies){
-      if(j!=k){
-        pred_res_file <- paste0("./real_data_pred/",pred_method,"_",i,"_trn_",j,"_tst_",k,".rds")
-        pred_res <- readRDS(pred_res_file)
-        crc_pred <- rbind(crc_pred,pred_res)
+ibd_df <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                     auc=numeric(0),accuracy=numeric(0),specificity=numeric(0),sensitivity=numeric(0))
+ibd_summ_df <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                          average_auc=numeric(0),average_accuracy=numeric(0), average_specificity=numeric(0),average_sensitivity=numeric(0))
+for(study1 in ibd_studies){
+  for(study2 in ibd_studies){
+    for(norm_method in norm_methods$norm_method){
+      if(study1 != study2){
+        # study1="FengQ_2015";study2="GuptaA_2019";norm_method="TSS";pred_method="ranger"
+		df <- readRDS(paste0("./real_data_pred/",pred_method,"_",norm_method,"_trn_",study1,"_tst_",study2,".rds"))
+        summ_df <- data.frame(study1=study1,study2=study2,norm_method=norm_method,pred_method=pred_method,
+                              average_auc=round(mean(as.numeric(df$auc_value)),3),
+                              average_accuracy=round(mean(as.numeric(df$accuracy)),3),
+                              average_specificity=round(mean(as.numeric(df$specificity)),3),
+                              average_sensitivity=round(mean(as.numeric(df$sensitivity)),3))
+        ibd_df <- rbind(ibd_df,df)
+        ibd_summ_df <- rbind(ibd_summ_df,summ_df)
       }
     }
   }
 }
-# save the results
-saveRDS(ibd_pred,"data/ibd_pred.rds")
+saveRDS(ibd_df,"data/ibd_pred.rds")
+saveRDS(ibd_summ_df,"data/ibd_pred_summ.rds")
 
-### compute the mean auc value for different combinations
-ibd_pred_summ <- data.frame(study1=NA,study2=NA,pred_method=NA,norm_method=NA,average_auc=NA)
-ibd_pred_summ <- ibd_pred_summ[-1,]
-for(i in unique(ibd_pred$study1)){
-  for(j in unique(ibd_pred$study2)){
-    for(k in unique(ibd_pred$norm_method)){
-      data <- ibd_pred[ibd_pred$study1==i & ibd_pred$study2==j & ibd_pred$norm_method==k, ]
-      df <- data.frame(study1=i,study2=j,pred_method="ibd",norm_method=k,
-                       average_auc=round(mean(as.numeric(data$auc_values)),5))
-      ibd_pred_summ <- rbind(ibd_pred_summ,df)
+
+### add the ranks according to the average auc, misclassification rate, specificity, sensitivity
+ibd_summ_final <- data.frame(study1=character(0),study2=character(0),norm_method=character(0),pred_method=character(0),
+                             average_auc=numeric(0),average_accuracy=numeric(0),average_specificity=numeric(0),average_sensitivity=numeric(0),
+                             order_auc=numeric(0),order_accuracy=numeric(0),order_specificity=numeric(0),order_sensitivity=numeric(0))
+for(study1 in ibd_studies){
+  for(study2 in ibd_studies){
+    if(study1 != study2){
+      df <- ibd_summ_df[ibd_summ_df$study1==study1 & ibd_summ_df$study2==study2,]
+      # orders according to the descending order of average auc
+      df <- df[order(df$average_auc,decreasing=TRUE),]
+      df$order_auc <- 1:nrow(df)
+      # orders according to the descending order of average accuracy
+      df <- df[order(df$average_accuracy,decreasing=TRUE),]
+      df$order_accuracy <- 1:nrow(df)
+      # orders according to the descending order of average specificity
+      df <- df[order(df$average_specificity,decreasing=TRUE),]
+      df$order_specificity <- 1:nrow(df)
+      # orders according to the descending order of average sensitivity
+      df <- df[order(df$average_sensitivity,decreasing=TRUE),]
+      df$order_sensitivity <- 1:nrow(df)
+      # combine the results
+      ibd_summ_final <- rbind(ibd_summ_final,df)
     }
   }
 }
-
-### add the orders of auc_values
-final_data <- data.frame(study1=NA,study2=NA,pred_method=NA,norm_method=NA,average_auc=NA,order_average_auc=NA)
-final_data <- final_data[-1,]
-for(i in unique(ibd_pred_summ$study1)){
-  for(j in unique(ibd_pred_summ$study2)){
-    # i="FengQ_2015";j="ZellerG_2014"
-    data <- ibd_pred_summ[ibd_pred_summ$study1==i & ibd_pred_summ$study2==j,]
-    # orders for average auc
-    data <- data[order(data$average_auc,decreasing=T),]
-    data$order_average_auc <- 1:nrow(data)
-    # final data
-    final_data <- rbind(final_data,data)
-  }
-}
-# save the results
-saveRDS(final_data,"data/ibd_pred_summ.rds")
+saveRDS(ibd_summ_final,"data/ibd_pred_summ.rds")
 
 
 
